af884b818d6f2ab912c65a6307f16534de39aba7
naman <guptanaman1995@gmail.com>
2023-12-14 23:03:45 +0530

    Setup basic connections b/w server and client

---
+++ public/js/chat.js
@@ -0,0 +1,12 @@
+const socket = io();
+
+document.querySelector('#message-form').addEventListener('submit',
+  (e) => {
+    e.preventDefault();
+    const message = e.target.elements.message.value;
+    socket.emit('sendMessage', message);
+  });
+
+socket.on('message', (message) => {
+  console.log(message);
+});
--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
 const app = express();
-
-
+const server = http.createServer(app);
+const io = socketio(server);
 
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
-
-
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+// server(emit)  --> client(receive)  countUpdated
+// client(emit) --> server(receive)  increment
+
+// socket.emit --> Send message to one client.
+// socket.broadcast.emit -> sends message to everyone except one sending it.
+
+// io.emit --> send messages to everyone.
+
+io.on('connection', (socket) => {
+  console.log('New web socket connection');
+
+  socket.emit('message', 'Welcome!');
+  socket.broadcast.emit('message', 'A new user has joined!');
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 })
