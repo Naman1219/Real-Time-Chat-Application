2d39278c7fc97146a9fcfa2ed7026a67596d0b4a
naman <guptanaman1995@gmail.com>
2023-12-15 01:40:44 +0530

    Added geo location feature

--- src/index.js
+++ src/index.js
@@ -33,6 +33,10 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     io.emit('message', 'A user has left');
   });
+
+  socket.on('sendLocation', (data) => {
+    io.emit('message', `https://google.com/maps?q=${data.latitude},${data.longitude}`);
+  });
 });
 
 server.listen(port, () => {
