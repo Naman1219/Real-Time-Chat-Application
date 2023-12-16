1a7748025db006598bf8ef9286717c583943ebc5
naman <guptanaman1995@gmail.com>
2023-12-17 00:36:20 +0530

    Added Rendering messages changes and bad-word library

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -26,16 +27,23 @@ io.on('connection', (socket) => {
   socket.emit('message', 'Welcome!');
   socket.broadcast.emit('message', 'A new user has joined!');
 
-  socket.on('sendMessage', (message) => {
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter()
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed!');
+    }
+
     io.emit('message', message);
+    callback();
   });
 
   socket.on('disconnect', () => {
     io.emit('message', 'A user has left');
   });
 
-  socket.on('sendLocation', (data) => {
-    io.emit('message', `https://google.com/maps?q=${data.latitude},${data.longitude}`);
+  socket.on('sendLocation', (coords, callback) => {
+    io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+    callback()
   });
 });
 
